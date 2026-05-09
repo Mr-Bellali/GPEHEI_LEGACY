2a4262f2d7886a8548850a2baf1f73614d8159c8
Yassine Bellali <bellali.yassine52@gmail.com>
2026-05-09 23:14:00 +0100

    deleted old teacher app

--- teacher/src/index.php
+++
@@ -1,3 +0,0 @@
-<?php 
-    include_once("views/templates/auth_template.php");
-?>
--- teacher/src/models/auth_model.php
+++
@@ -1,4 +0,0 @@
-<?php
-    // Define the class of authentication (it will have login and change password for sure, and maybe reset password)
-    
-?>
