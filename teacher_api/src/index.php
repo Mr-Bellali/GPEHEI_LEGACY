3169ff622d832f4de53f11370205f85edbbd6fac
Yassine Bellali <bellali.yassine52@gmail.com>
2026-05-10 13:15:40 +0100

    import all the necessary files

--- teacher_api/src/index.php
+++ teacher_api/src/index.php
@@ -1,57 +1,7 @@
 <?php
-header("Content-Type: application/json");
-include 'config/db.php';
-
-$db = Database::getInstance();
-
-$method = $_SERVER['REQUEST_METHOD'];
-$input = json_decode(file_get_contents('php://input'), true);
-
-switch ($method) {
-    case 'GET':
-        $db->query("select * from teacher");
-        echo json_encode($db->resultSet());
-        break;
-    case 'POST':
-        // handlePost($pdo, $input);
-        break;
-    case 'PUT':
-        // handlePut($pdo, $input);
-        break;
-    case 'DELETE':
-        // handleDelete($pdo, $input);
-        break;
-    default:
-        echo json_encode(['message' => 'Invalid request method']);
-        break;
-}
-
-function handleGet($pdo) {
-    $sql = "SELECT * FROM users";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($result);
-}
-
-function handlePost($pdo, $input) {
-    $sql = "INSERT INTO users (name, email) VALUES (:name, :email)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(['name' => $input['name'], 'email' => $input['email']]);
-    echo json_encode(['message' => 'User created successfully']);
-}
-
-function handlePut($pdo, $input) {
-    $sql = "UPDATE users SET name = :name, email = :email WHERE id = :id";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(['name' => $input['name'], 'email' => $input['email'], 'id' => $input['id']]);
-    echo json_encode(['message' => 'User updated successfully']);
-}
-
-function handleDelete($pdo, $input) {
-    $sql = "DELETE FROM users WHERE id = :id";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(['id' => $input['id']]);
-    echo json_encode(['message' => 'User deleted successfully']);
-}
-?>
+require_once 'config/db.php';
+require_once 'middleware/authMiddleware.php';
+require_once 'model/teacher.php';
+require_once 'service/teacherService.php';
+require_once 'controller/teacherController.php';
+require_once 'router/api.php';
